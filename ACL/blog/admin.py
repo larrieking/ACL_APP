ddfa37ddfb9d6524998a0b8dcdff90692acde949
larrieking <larrie4christ@gmail.com>
2022-08-07 00:29:05 +0100

    first commit

--- ACL/blog/admin.py
+++ ACL/blog/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 admin.site.register(Category)
 # Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status', 'img')
--- ACL/blog/views.py
+++ ACL/blog/views.py
@@ -2,15 +2,53 @@
 
 # Create your views here.
 from django.shortcuts import  render, get_object_or_404
-from .models import Post, Category
+from .models import Post, Category, Comment
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def post_list(request):
-    posts = Post.published.all().order_by('publish')
-    cover = posts[len(posts)-1]
-    categories = Category.objects.all()
 
-    return render(request, 'list.html', {'posts':posts, 'cover':cover, 'categories':categories})
+
+    posts = Post.published.all()
+    paginator = Paginator(posts,4)
+    page = request.GET.get('page')
+    cover = posts[len(posts)-1]
+
+
+    try:
+        postss = paginator.page(page)
+    except PageNotAnInteger:
+        postss = paginator.page(1)
+    except EmptyPage:
+        postss = paginator.page(paginator.num_pages)
+
+
+    return render(request, 'list.html', {'posts':postss, 'cover':cover,  'page':page})
 
 def post_details(request, year, month, day, post):
-    post = get_object_or_404(Post, slug = post, status='published', publish_year=year, publish_month=month, publish_day=day )
-    return render(request, 'detail.html', {'post':post})
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    body = request.POST.get('body')
+
+
+
+    post = get_object_or_404(Post, slug = post, status='published', publish__year=year, publish__month=month, publish__day=day )
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+
+
+    if request.method == "POST":
+        c1 = Comment(name=name, email=email, body=body, post = post)
+
+        c1.save()
+        print(comments)
+
+
+    return render(request, 'detail.html', {'post':post,  "comments":comments})
+
+def create_comment(request):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    body = request.POST.get('body')
+    print(body)
+    return render(request, 'detail.html')
